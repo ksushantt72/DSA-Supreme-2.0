34d1a5cdc31b8d707096cb669761902d032f2a3d
ksushantt72 <ksushantt72@gmail.com>
2024-01-15 23:18:04 +0530

    commit

---
+++ week 3 Array/Assignment/FindFirstRepeatingELement.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int isRepeated(int*arr,int n){
+    vector<int>hash(n,0);
+    //n size ki array and all are initialised with 0
+
+    for(int i=0;i<n;i++){
+        
+    }
+}
+
+
+int main(){
+    int arr[]={1,5,3,4,3,5,6};
+    int n=sizeof(arr)/sizeof(int);
+}
