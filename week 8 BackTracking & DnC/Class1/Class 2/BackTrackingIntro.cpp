811d9953f2d49207e2d7702b49251b61cbfe4256
ksushantt72 <ksushantt72@gmail.com>
2024-02-13 15:51:45 +0530

    Backtracking Updated

--- week 8 BackTracking & DnC/Class1/Class 2/BackTrackingIntro.cpp
+++ week 8 BackTracking & DnC/Class1/Class 2/BackTrackingIntro.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 using namespace std;
 //Function declaration
-void PrintPermutation(string str,int index){
+
+//This is the Function in which we have passed the string by value
+//So this will take extra memory
+
+void PrintPermutationValue(string str,int index){
     if(index>=str.length()){
         //Print String 
         cout << str << " ";
@@ -11,7 +15,35 @@ void PrintPermutation(string str,int index){
     for(int j=index;j<str.length();j++){
         swap(str[index],str[j]);
         //Recursion 
-        PrintPermutation(str,index+1);
+        PrintPermutationValue(str,index+1);
+    }
+
+}
+
+//This is the same function of Print Permutation where we have passed 
+//string by reference
+
+void PrintPermutationReference(string &str,int index){
+    if(index>=str.length()){
+        //Print String 
+        cout << str << " ";
+        return;
+    }
+
+    for(int j=index;j<str.length();j++){
+        swap(str[index],str[j]);
+        //Recursion 
+        PrintPermutationReference(str,index+1);
+        //Since we have passed it by reference then on return call we have to 
+        //swap it again as original string because on passing by reference 
+        //all the changes in current string changes the main string too 
+
+        //So here comes Backtracking where we will do our work by first swapping and
+        //when our work is done we will swap it again so that non change is done to 
+        //original string
+
+        swap(str[index],str[j]);   //--------->> BackTracking
+        
     }
 
 }
@@ -21,5 +53,10 @@ int main(){
     string str="abc";
     int index=0;
     //Function Call
-    PrintPermutation(str,index);
+    //Pass by value (doesnot require Backtracking  ut takes extra memory)
+    PrintPermutationValue(str,index);
+    cout << endl;
+
+    //Pass by Reference (Requires Backtracking  but doesnot take extra memory)
+    PrintPermutationReference(str,index);
 }
