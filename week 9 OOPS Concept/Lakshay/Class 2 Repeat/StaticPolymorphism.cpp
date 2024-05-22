f062e11268f5154d44b663c4b9839d51cdb8558f
ksushantt72 <ksushantt72@gmail.com>
2024-05-22 23:59:22 +0530

    changes implemented

--- week 9 OOPS Concept/Lakshay/Class 2 Repeat/StaticPolymorphism.cpp
+++ week 9 OOPS Concept/Lakshay/Class 2 Repeat/StaticPolymorphism.cpp
@@ -13,38 +13,49 @@ public:
 
     Vector(int x, int y) : x(x), y(y) {} // constructor using init list
 
-    // void operator+(const Vector &src)
-    // {
-    //     this->x = this->x + src.x;
-    //     this->y = this->y + src.y;
-    // }
-    // void operator-(const Vector &src)
-    // {
-    //     this->x = this->x - src.x;
-    //     this->y = this->y - src.y;
-    // }
+    void operator+(const Vector &src)
+    {
+        this->x = this->x + src.x;
+        this->y = this->y + src.y;
+    }
+    void operator-(const Vector &src)
+    {
+        this->x = this->x - src.x;
+        this->y = this->y - src.y;
+    }
 
-    // void operator++(const Vector &src)
-    // {
-    //     this->x++;
-    //     this->y++;
-    // }
+    void operator++()
+    {
+        this->x++;
+        this->y++;
+    }
 
-    // void display()
-    // {
-    //     cout << "x:" << x << "  " << "y:" << y << endl;
-    // }
+    void display()
+    {
+        cout << "x:" << x << "  " << "y:" << y << endl;
+    }
 };
 
 int main()
 {
     Vector v1(3, 5);
     Vector v2(4, 6);
+    Vector v3(4, 6);
+
 
     // v1 + v2;
     // v1.display();
 
     // v1 - v2;
     // v1.display();
+
     // ++v1;
+    // v1.display();
+    
+    // ++v2;
+    // v2.display();
+
+    (v3+v2);
+    v3.display();
+
 }
