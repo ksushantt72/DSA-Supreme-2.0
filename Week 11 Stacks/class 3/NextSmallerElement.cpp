0333eb4ec160262b34a4c439b00d50f761fb9e7e
ksushantt72 <ksushantt72@gmail.com>
2024-06-08 20:50:40 +0530

    Coded on 8th-June-24

---
+++ Week 11 Stacks/class 3/NextSmallerElement.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+vector<int> NextSmaller(vector<int>&ans,int arr[],int size){
+    //we will create a stack for storing smaller elements
+    stack<int>st;
+    //now we will insert the first bottom element as -1
+    //because while traversal from the right side to the left,
+    //rightmost element doesnot have next element
+    //so in that case we will initialise -1
+    st.push(-1);
+
+    //Now using "for" loop for the traversal
+    for(int i=size-1;i>=0;i--){
+        int curr=arr[i];
+        //now we will compare the curr element with the 
+        //top element of the stack
+        //jab tk top ka element g reater hai curr se tbt tk pop hoga
+        while(st.top()>=curr){
+            st.pop();
+        } 
+        //here, our top element is lesser than the curr element
+        //so we will insert the top element of stack in the ans array
+        //and we will push curr element in the stack for the further comparison
+        //becuase it can be the smaller element for next iteration 
+        ans[i]=st.top();
+        st.push(curr);
+    }
+    // when we are out of the for loop , we will have our answer array created according to 
+    //our requirement
+    return ans;
+}
+
+
+int main(){
+    int arr[]={4,1,6,3};
+    int size=sizeof(arr)/sizeof(int);
+
+    //creating a vector of specific size
+    vector<int>ans(size);
+    ans=NextSmaller(ans,arr,size);
+
+    //Printing the array
+    for(auto i:ans){
+        cout << i << " ";
+    }
+    cout << endl;
+}
---
+++ Week 11 Stacks/class 3/PrevSmaller.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+void PrevSmaller(vector<int>&ans,int arr[],int size){
+    //Creating a stack for storing the previous Smaller element
+    stack<int>st;
+    st.push(-1);
+    //for loop for the traversal on the array 
+    for(int i=0;i<size;i++){
+        int currElement=arr[i];
+        //Now comparison
+        while(st.top()>=currElement){
+            //jab tk stack ka top element greater hai
+            //curr element se tbtk pop kr do stack
+            //so that we will get previous smaller element
+            //Note: aage badhte waqt hm piche ka smaller store krte hain stack me
+            //taaki mujhe baar baar same element ko compare na karna pare
+            st.pop();
+        }
+        //when we are here, it means stack has smaller element which lies  before the array's curr. Index
+        //is in the stack's top position 
+        ans[i]=st.top();
+        //Now we will push the curr element in the stack so that it can be compared in the firther traversal 
+        st.push(currElement);
+    }
+    // cout << ans[0];
+
+}
+
+int main(){
+    int arr[]={1,2,3,4};
+    int size=sizeof(arr)/sizeof(int);
+    
+    
+    //creating ans array for storing answer
+    vector<int>ans(size);
+    PrevSmaller(ans,arr,size);
+
+    for(auto i:ans){
+        cout << i << " ";
+    }
+    cout << endl;
+
+
+
+
+
+}
