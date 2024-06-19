fd2ff15b4bfd5959e7715a2c988ba1d49c4e1f3e
ksushantt72 <ksushantt72@gmail.com>
2024-06-19 09:53:19 +0530

    Changes

--- Week 12 Queue/class 1 Repeat/queueImplementation.cpp
+++ Week 12 Queue/class 1 Repeat/queueImplementation.cpp
@@ -14,27 +14,58 @@ public:
     {
         arr = new int[size];
         this->size = size;
-        front = -1;  
+        front = -1;
         rear = -1;
     }
-    
 
-    void push(int value){
-        //we will check if the Queue is full or not--->> OVERFLOW
-        if(rear=size-1){
+    void push(int value)
+    {
+        // we will check if the Queue is full or not--->> OVERFLOW Checking
+        if (rear = size - 1)
+        {
             cout << " Queue is Full , Can't Push" << endl;
             return;
         }
-        //Check If the Queue is empty or not
-        else if(front==-1 && rear==-1){
+        // Check If the Queue is empty or not
+        else if (front == -1 && rear == -1)
+        {
             front++;
             rear++;
-            arr[rear]=value;
+            arr[rear] = value;
         }
-        //NormalCase
-        else{
+        // NormalCase
+        else
+        {
             rear++;
-            arr[rear]=value;
+            arr[rear] = value;
+        }
+    }
+    void pop(){
+        //check UnderFlowCase
+        if(front==-1 && rear==-1){
+            cout << " Queue UnderFLow " << endl;
+            return;
+        }
+        //if there is single element
+        else if(front==rear){
+            arr[front]=-1;
+            front=-1;
+            rear=-1;
+            
+        }
+        //Normal Case
+        else {
+            arr[front]=-1;
+            front++;
+        }
+    }
+
+    bool isEmpty(){
+        if(front==-1 && rear==-1){
+            return true;
+        }
+        else{
+            return false;
         }
     }
 };
