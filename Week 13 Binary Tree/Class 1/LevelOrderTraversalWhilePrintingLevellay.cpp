e567949da081579f2ce05e4112969be7748234bd
ksushantt72 <ksushantt72@gmail.com>
2024-06-29 19:07:19 +0530

    Changes in this code

--- Week 13 Binary Tree/Class 1/LevelOrderTraversalWhilePrintingLevellay.cpp
+++ Week 13 Binary Tree/Class 1/LevelOrderTraversalWhilePrintingLevellay.cpp
@@ -1,48 +1,104 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node*left;
-    Node*right;
+    Node *left;
+    Node *right;
 
-    //Constructor
-    Node(int data){
-        this->data=data;
-        this->left=nullptr;
-        this->right=nullptr;
+    // Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->left = nullptr;
+        this->right = nullptr;
     }
 };
 
-
-Node*CreateTree(){
+Node *CreateTree()
+{
     cout << "Enter the Value to be Entered: ";
     int data;
     cin >> data;
 
-    if(data==-1){
+    if (data == -1)
+    {
         return nullptr;
     }
 
-    Node*root=new Node(data);
+    Node *root = new Node(data);
 
-    //Create Left Subtree
-    cout << "For Left of Node: "<< root->data << ", ";
-    root->left=CreateTree();
+    // Create Left Subtree
+    cout << "For Left of Node: " << root->data << ", ";
+    root->left = CreateTree();
 
-    cout << "For Right of Node: "<< root->data << ", ";
-    root->right=CreateTree();
+    cout << "For Right of Node: " << root->data << ", ";
+    root->right = CreateTree();
 
     // ye root pichle recursive call ke left aur right se jaake attach hoga
-    //so must not forget about this important line
+    // so must not forget about this important line
     return root;
 }
 
+void PreorderTraversal(Node *&rootNode)
+{
+    queue<Node *> q;
+    //Base Case
+    if(rootNode==nullptr){
+        return;
+    }
+    //Printing the Current Value in case of PreOrderTraversal
+    cout << rootNode->data << "->";
+    PreorderTraversal(rootNode->left);
+    PreorderTraversal(rootNode->left);
+
+}
+void inOrderTraversal(Node *&rootNode)
+{
+    queue<Node *> q;
+    //Base Case
+    if(rootNode==nullptr){
+        return;
+    }
+    //we will print in order
+    inOrderTraversal(rootNode->left);
+    cout << rootNode->data << "->";
+    inOrderTraversal(rootNode->right);
+}
+void postOrderTraversal(Node *&rootNode)
+{
+    queue<Node *> q;
+    //Base Case
+    if(rootNode==nullptr){
+        return;
+    }
+    //We will print after the Node's left and right Elements are printed
+    
+    postOrderTraversal(rootNode->left);
+    cout << rootNode->data << "->";
+    postOrderTraversal(rootNode->right);
+}
 
 int main()
 {
-    Node*rootNode=CreateTree();
-    
+    Node *root = CreateTree();
+
+    cout << "Printing using Traversals" << endl;
+
+    cout << "Preorder Traversal: ";
+    PreorderTraversal(root);
+    cout << endl;
+
+    cout << "inOrder Traversal: ";
+    inOrderTraversal(root);
+    cout << endl;
+
+    cout << "PostOrderTraversal: ";
+    postOrderTraversal(root);
+    cout << endl;
+
     return 0;
 }
