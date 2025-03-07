0783047832a62f1ce4963dca9a52c4145303a7c6
ksushantt72 <ksushantt72@gmail.com>
2025-03-08 02:12:27 +0530

    Day1

--- week 3 Array/class 2 Vector/SortZeroOneAgain.cpp
+++ week 3 Array/class 2 Vector/SortZeroOneAgain.cpp
@@ -2,43 +2,53 @@
 #include <vector>
 using namespace std;
 
-//Creating an array to print the Array  ELements
-void PrintArray(vector<int>arr){
-    int n=arr.size();
-    for(int i:arr){
+// Creating an array to print the Array  ELements
+void PrintArray(vector<int> arr)
+{
+    int n = arr.size();
+    for (int i : arr)
+    {
         cout << i << " ";
     }
 }
-void SortArray(vector<int>&arr){
-    int n=arr.size();
+void SortArray(vector<int> &arr)
+{
+    int n = arr.size();
     int start = 0;
-    int end = n-1;
-    int i=0;
-    //we will sort in a way such that the zero will be in starting and 1 will be in ending
-    while(i!=end){
-        if(arr[i]==0){
-            //agar 0 milega to usko start se swap krke  index i ko increase krke aage badha denge
-            swap(arr[i],arr[start]);
+    int end = n - 1;
+    int i = 0;
+    // we will sort in a way such that the zero will be in starting and 1 will be in ending
+    while (i != end)
+    {
+        if (arr[i] == 0)
+        {
+            // agar 0 milega to usko start se swap krke  index i ko increase krke aage badha denge
+            swap(arr[i], arr[start]);
             i++;
             start++;
         }
-        else{
-            //in else case we are getting 1
-            //so we will swap it with end and decrease it everytime
-            swap(arr[i],arr[end]);
+        else
+        {
+            // in else case we are getting 1
+            // so we will swap it with end and decrease it everytime
+            swap(arr[i], arr[end]);
             end--;
-            //but we will not increase i because the i needs to be checked again coz we dont know 
-            //what value has been swapped
-            
+            // but we will not increase i because the i needs to be checked again coz we dont know
+            // what value has been swapped with
+            // so to recheck it we will not increase i
         }
     }
 }
 
-
-
 int main()
 {
-    vector<int>arr{1,0,0,1,1,1,0,0,1,0};
+    vector<int> arr{1, 0, 0, 1, 1, 1, 0, 0, 1, 0};
+    cout << "Printing the Array Before Sorting:" << endl;
     PrintArray(arr);
+
+    SortArray(arr);
+    cout << "Printing the Array After Sorting:" << endl;
+    PrintArray(arr);
+
     return 0;
 }
